c0618a79ff2af4abb7694b0856e7094e79ecfa6c
Valuka gimhan <valukagimhan@gmail.com>
2022-09-05 10:45:08 +0530

    1

---
+++ section1/magic number.py
@@ -0,0 +1,13 @@
+import random
+
+mgnu= random.randint(0, 10)
+print("Enter your lucky number between 0-10 :")
+num = int(input())
+if (num == mgnu):
+    {
+        print("Congratulations you won!!!")
+    }
+else :
+    {
+        print("Winning number is",mgnu ,"Better luck next time.Try again!")
+    }
