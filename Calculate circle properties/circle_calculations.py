f261f52839286a267a81256e3ede424bdbb17550
Valuka gimhan <valukagimhan@gmail.com>
2022-10-29 18:34:15 +0530

    v1

---
+++ Calculate circle properties/circle_calculations.py
@@ -0,0 +1,8 @@
+PI = 22/7
+r = float(input("Enter the radius of a circle:"))
+diameter = r * 2
+print("Diameter of circle = %.2f"%diameter)
+circumference = 2 * PI * r
+print("Circumference of circle = %.2f"%circumference)
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
